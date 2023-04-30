8da7f1ad0c289bd53d3e1f62d93361861c6f464b
juanarayafonsec <juan-jose95@hotmail.com>
2023-04-30 19:09:40 +0200

    CommandHandler added

--- SM-Post/Post.Cmd/Post.Cmd.Api/Extensions/ApplicationServicesExtension.cs
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Extensions/ApplicationServicesExtension.cs
@@ -1,6 +1,10 @@
 using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
 using CQRS.Core.Infrastructure;
+using Post.Cmd.Api.Commands;
+using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
 
@@ -13,5 +17,7 @@ public static class ApplicationServicesExtension
         services.Configure<MongoDbConfig>(config.GetSection(nameof(MongoDbConfig)));
         services.AddScoped<IEventStoreRepository, EventStoreRepository>();
         services.AddScoped<IEventStore, EventStore>();
+        services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+        services.AddScoped<ICommandHandler, CommandHandler>();
     }
 }
