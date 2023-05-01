e3bf5a56991eeb86222e1748a44d639b803606a0
juanarayafonsec <juan-jose95@hotmail.com>
2023-05-01 17:24:17 +0200

    Event consumer added

---
+++ CQRS-ES/CQRS.Core/Consumers/EventConsumer.cs
@@ -0,0 +1,6 @@
+namespace CQRS.Core.Consumers;
+
+public class EventConsumer
+{
+       
+}
--- SM-Post/Post.Query/Post.Query.Api/Extensions/ApplicationConfigurationExtension.cs
+++ SM-Post/Post.Query/Post.Query.Api/Extensions/ApplicationConfigurationExtension.cs
@@ -1,9 +1,12 @@
-using System.Diagnostics.CodeAnalysis;
+using Confluent.Kafka;
+using CQRS.Core.Consumers;
 using Microsoft.EntityFrameworkCore;
 using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.Repository;
+using EventConsumer = Post.Query.Infrastructure.Consumers.EventConsumer;
 using EventHandler = Post.Query.Infrastructure.Handlers.EventHandler;
 
 namespace Post.Query.Api.Extensions;
@@ -16,6 +19,9 @@ public static class ApplicationConfigurationExtension
         services.AddScoped<IPostRepository, PostRepository>();
         services.AddScoped<ICommentRepository, CommentRepository>();
         services.AddScoped<IEventHandler, EventHandler>();
+        services.Configure<ConsumerConfig>(config.GetSection(nameof(ConsumerConfig)));
+        services.AddScoped<IEventConsumer, EventConsumer>();
+        services.AddHostedService<ConsumerHostedService>();
     }
 
     private static void AddDbConfiguration(this IServiceCollection services, IConfiguration config)
